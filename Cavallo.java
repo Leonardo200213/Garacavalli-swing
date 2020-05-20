80833db0f945546d61a6cdfc7ea72411ad6ea469
Leonardo200213 <leomesseri98@gmail.com>
2020-05-20 17:26:08 +0200

    aggiunta classe cavallo in java

---
+++ Cavallo.java
@@ -0,0 +1,42 @@
+package tipsit.garacavalliconthread;
+
+/**
+ * @author Messeri
+ */
+
+public class Cavallo implements Runnable {
+    GUICavalli c;
+    Gara campo;
+    Thread t = new Thread(this); //dichiarazione del thread
+    int posizione; //posizione del cavallo
+    int velocità; //velocità del cavallo
+    int conteggio;
+    
+    public Cavallo(GUICavalli horse, Gara cam){
+        c = horse;
+        campo = cam;
+        conteggio = 0;
+        velocità = 5; //impostazione velocità del cavallo
+        posizione = 0; //impostazione della posizione iniziale del cavallo
+        t.start(); //comincia il thread
+    }
+    
+    public void run(){
+        int dimImmagine = 10;
+        int dimPista = 1000;
+        while((c.getX() + dimImmagine) < dimPista){ //finchè l'immagine è dentro la pista, continua
+            if ((conteggio % 5) == 0){
+                velocità = (int)(Math.random() * 5 + 1); //velocità del cavallo
+            }
+            c.setX(c.getX() + velocità); //aggiunge alla coordinata x del cavallo la velocità per farlo muovere
+            conteggio++;
+            try {
+                Thread.sleep(75);
+            }
+            catch (Exception e) {}
+            campo.repaint();
+        }
+        posizione = campo.getPosizione(); //mette come posizione quella nella classe campo
+        campo.arrivi(); //annuncia gli arrivi
+    }
+}
